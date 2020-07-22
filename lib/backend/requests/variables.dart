6998a2e86a2c9b4dd3e6e2abf947792d307ef686
mrx123 <mrx12335@users.noreply.github.com>
2020-07-22 14:32:22 +0200

    Das ist ein Test Cum it. 8==>

--- lib/backend/requests/variables.dart
+++ lib/backend/requests/variables.dart
@@ -1,5 +1,5 @@
 // -------------------------------- Imports ---------------------------------
-
+import 'package:path_provider/path_provider.dart';
 import '../objects.dart' as objects;
 import 'dart:io';
 
@@ -46,9 +46,8 @@ abstract class FileHandler {
 
   /// Stellt den Dateipfad der Applikation zur Verfügung
   static Future<String> get _localPath async {
-    //TODO einkommentieren bei einem Flutter Projekt
-//    final directory=await getApplicationDocumentsDirectory();
-//    return directory.path;
+    final directory=await getApplicationDocumentsDirectory();
+    return directory.path;
   }
 
   /// Erstellt das File
